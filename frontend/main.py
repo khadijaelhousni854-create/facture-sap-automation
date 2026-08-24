eeb17039da602fabbbebeb4f21f0c69c1d4231a5
fadwa <fednnaouri@gmail.com>
2026-08-24 10:12:19 +0100

    done

--- frontend/main.py
+++ frontend/main.py
@@ -50,6 +50,3 @@ async def main():
         await fermer_navigateur(playwright, navigateur)
         logger.info("=== Fin du traitement ===")
 
-
-if __name__ == "__main__":
-    asyncio.run(main())
